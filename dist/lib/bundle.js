63392c69fb1fe849db5cc90297b99f4f7f71f4d6
Andrew Schumacher <as228512@ohio.edu>
2018-10-12 21:57:10 -0400

    updated css + start of logic for toggling tile glow upon game start

--- dist/lib/bundle.js
+++ dist/lib/bundle.js
@@ -84,13 +84,23 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.reset = undefined;
+exports.startReset = undefined;
 
 var _tile = __webpack_require__(/*! ./tile.js */ "./lib/tile.js");
 
-var reset = exports.reset = function reset() {
+var toggleStartButton = function toggleStartButton(requestType) {
+  requestType === "start" ? document.getElementById("start-button").innerHTML = "Reset" : document.getElementById("start-button").innerHTML = "Start";
+};
+
+var startReset = exports.startReset = function startReset() {
   document.getElementById("start-button").onclick = function () {
     (0, _tile.createTiles)();
+    toggleStartButton("start");
+
+    document.querySelectorAll("#tiles li").forEach(function (li) {
+      li.addEventListener("mouseover", _tile.toggleTileActivation);
+      li.addEventListener("mouseout", _tile.toggleTileActivation);
+    });
   };
 };
 
@@ -112,7 +122,7 @@ var _board = __webpack_require__(/*! ./board.js */ "./lib/board.js");
 
 document.addEventListener("DOMContentLoaded", function () {
   (0, _tile.createTiles)();
-  (0, _board.reset)();
+  (0, _board.startReset)();
 });
 
 /***/ }),
@@ -135,31 +145,37 @@ var sample = function sample(array) {
 };
 
 var tiles = exports.tiles = {
-  1: ['R', 'I', 'F', 'O', 'B', 'X'],
-  2: ['I', 'F', 'E', 'H', 'E', 'Y'],
-  3: ['D', 'E', 'N', 'O', 'W', 'S'],
-  4: ['U', 'T', 'O', 'K', 'N', 'D'],
+  1: ["R", "I", "F", "O", "B", "X"],
+  2: ["I", "F", "E", "H", "E", "Y"],
+  3: ["D", "E", "N", "O", "W", "S"],
+  4: ["U", "T", "O", "K", "N", "D"],
 
-  5: ['H', 'M', 'S', 'R', 'A', 'O'],
-  6: ['L', 'U', 'P', 'E', 'T', 'S'],
-  7: ['A', 'C', 'I', 'T', 'O', 'A'],
-  8: ['Y', 'L', 'G', 'K', 'U', 'E'],
+  5: ["H", "M", "S", "R", "A", "O"],
+  6: ["L", "U", "P", "E", "T", "S"],
+  7: ["A", "C", "I", "T", "O", "A"],
+  8: ["Y", "L", "G", "K", "U", "E"],
 
-  9: ['Qu', 'B', 'M', 'J', 'O', 'A'],
-  10: ['E', 'H', 'I', 'S', 'P', 'N'],
-  11: ['V', 'E', 'T', 'I', 'G', 'N'],
-  12: ['B', 'A', 'L', 'I', 'Y', 'T'],
+  9: ["Qu", "B", "M", "J", "O", "A"],
+  10: ["E", "H", "I", "S", "P", "N"],
+  11: ["V", "E", "T", "I", "G", "N"],
+  12: ["B", "A", "L", "I", "Y", "T"],
 
-  13: ['E', 'Z', 'A', 'V', 'N', 'D'],
-  14: ['R', 'A', 'L', 'E', 'S', 'C'],
-  15: ['U', 'W', 'I', 'L', 'R', 'G'],
-  16: ['P', 'A', 'C', 'E', 'M', 'D']
+  13: ["E", "Z", "A", "V", "N", "D"],
+  14: ["R", "A", "L", "E", "S", "C"],
+  15: ["U", "W", "I", "L", "R", "G"],
+  16: ["P", "A", "C", "E", "M", "D"]
 };
 
 var createTiles = exports.createTiles = function createTiles() {
   for (var i = 1; i < 17; i++) {
-    document.getElementById('t' + i).innerHTML = sample(tiles[i]);
+    document.getElementById("t" + i).innerHTML = sample(tiles[i]);
   }
+};
+
+var toggleTileActivation = exports.toggleTileActivation = function toggleTileActivation(e) {
+  var li = e.target;
+
+  if (li.className === "false") li.className = "focused";else li.className = "false";
 };
 
 /***/ })
